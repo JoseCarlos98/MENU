45f6446504e3f7ed99e23d6cf3265e7ca4f3fa91
Jose Carlos LB <Joslos_carlos@hotmail.com>
2025-07-01 16:55:50 -0600

    Avance

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
-import { Module, NavigationService } from './service/navigation.service';
+import { Component, HostBinding, OnInit } from '@angular/core';
+import { NavigationService, Module, Section } from './service/navigation.service';
 import { CommonModule } from '@angular/common';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.html',
-  styleUrl: './app.scss',
+  standalone: true,
   imports: [
     CommonModule,
     RouterModule,
@@ -22,14 +22,17 @@ import { MatListModule } from '@angular/material/list';
     MatSidenavModule,
     MatToolbarModule,
     MatListModule,
+    MatExpansionModule,
     RouterOutlet
   ],
+  templateUrl: './app.html',
+  styleUrls: ['./app.scss']
 })
 export class App implements OnInit {
-  protected title = 'menu';
   modules: Module[] = [];
   activeModule: Module | null = null;
   isSidebarOpen = true;
+  expandedSections: {[key: string]: boolean} = {};
 
   constructor(private navigationService: NavigationService) { }
 
@@ -37,6 +40,12 @@ export class App implements OnInit {
     this.modules = this.navigationService.getModules();
     this.navigationService.activeModule$.subscribe(module => {
       this.activeModule = module;
+      // Inicializar todos los sections como expandidos
+      // if (module) {
+      //   module.sections?.forEach(section => {
+      //     this.expandedSections[section.id] = true;
+      //   });
+      // }
     });
   }
 
@@ -46,5 +55,9 @@ export class App implements OnInit {
 
   returnToMainMenu() {
     this.navigationService.setActiveModule(null);
+  }
+
+  toggleSection(sectionId: string) {
+    this.expandedSections[sectionId] = !this.expandedSections[sectionId];
   }
 }
